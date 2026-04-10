5e2868d45a3524f5b377ee2225be21cb210df32d
minhnhutvo <vovo07722@gmail.com>
2026-04-10 00:31:14 -0400

    edit button to profile page

--- js/afterLogin.js
+++ js/afterLogin.js
@@ -8,5 +8,17 @@ window.addEventListener("load", function () {
         buttonLogin.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-icon lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>`
         buttonLogin.href = "/profile.html"
         buttonLogin.classList.add("user-icon")
+        const buttonLogOut = document.createElement("a");
+        buttonLogOut.classList.add("btn-login");
+        buttonLogOut.innerText = "Log Out"
+        const headerActions = document.querySelector(".header-actions")
+
+        headerActions.appendChild(buttonLogOut)
+
+        buttonLogOut.addEventListener("click", function () {
+            localStorage.removeItem("currentUser");
+            window.location.reload();
+        })
+
     }
 })
---
+++ js/profile.js
@@ -0,0 +1,50 @@
+const nameValue = document.querySelector(".name-value");
+const email = document.querySelector(".email-value");
+const user = JSON.parse(localStorage.getItem("currentUser"))
+
+nameValue.textContent = user.fullName;
+email.textContent = user.email;
+const viewMode = document.querySelector(".view-mode");
+const editMode = document.querySelector(".edit-mode");
+const inputName = document.getElementById("input-name");
+const inputEmail = document.getElementById("input-email");
+const btnSave = document.getElementById("button-save");
+const btnCancel = document.getElementById("button-cancel");
+
+const btnEdit = document.getElementById("btn-edit");
+btnEdit.addEventListener("click", function (e) {
+    e.preventDefault();
+    btnEdit.style.display = "none";
+    viewMode.classList.add("d-none");
+    editMode.classList.remove("d-none");
+    inputName.value = user.fullName;
+    inputEmail.value = user.email;
+});
+btnSave.addEventListener("click", function (e) {
+    e.preventDefault();
+    let listUser = JSON.parse(localStorage.getItem("listUser")) || [];
+    const userIndex = listUser.findIndex(u => u.email === user.email);
+    listUser[userIndex].fullName = inputName.value;
+    listUser[userIndex].email = inputEmail.value;
+    localStorage.setItem("listUser", JSON.stringify(listUser));
+
+    user.fullName = inputName.value;
+    user.email = inputEmail.value;
+    localStorage.setItem("currentUser", JSON.stringify(user));
+    alert("Update Successfully");
+
+    nameValue.textContent = user.fullName;
+    email.textContent = user.email;
+    editMode.classList.add("d-none");
+    viewMode.classList.remove("d-none");
+    btnEdit.style.display = "";
+
+
+})
+btnCancel.addEventListener("click", function (e) {
+    e.preventDefault();
+    editMode.classList.add("d-none");
+    viewMode.classList.remove("d-none");
+    btnEdit.style.display = "";
+
+})
